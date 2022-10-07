585b4ec23cdfb5042a27c43d00d30ac3b5743107
Lucas Henrique <lukas.paes18@gmail.com>
2022-10-07 17:00:26 -0300

    fix: fix testes typography

--- src/components/atomic/Typography/Typography.test.tsx
+++ src/components/atomic/Typography/Typography.test.tsx
@@ -1,15 +1,23 @@
 import 'react-native';
 import React from 'react';
-import {Typography} from './Typography';
+import {CustomText} from './style';
 import {render} from '@testing-library/react-native';
 import {TypographyStyle} from '../../../common/';
 
+const mockStyleFontSize = {
+  fontWight: 700,
+};
+
 describe('Name of the group', () => {
-  it('renders default correctly Typography', () => {
-    render(<Typography>Eu</Typography>);
+  it('renders default correctly CustomText', () => {
+    render(<CustomText>Eu</CustomText>);
   });
 
-  it('renders default correctly Typography ', () => {
-    render(<Typography {...TypographyStyle}>Eu</Typography>);
+  it('renders default correctly CustomText ', () => {
+    render(<CustomText {...TypographyStyle}>Eu</CustomText>);
+  });
+
+  it('renders default correctly CustomText ', () => {
+    render(<CustomText {...mockStyleFontSize}>Eu</CustomText>);
   });
 });
