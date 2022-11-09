4f69fa43d6e6bae1729a56fa53121c4c1371cf31
Lucas Henrique <lukas.paes18@gmail.com>
2022-11-09 01:37:32 -0300

    feat: adicionado novas features

--- src/assets/icons/markerMapPointRecycleIcon/index.ts
+++ src/assets/icons/markerMapPointRecycleIcon/index.ts
@@ -3,7 +3,10 @@ import OrganicPoint from './organic-point.png';
 import PaperPoint from './paper-point.png';
 import PlasticPoint from './plastic-point.png';
 import GlassPoint from './glass.point.png';
-import EletronicPoint from './electronic-point.png';
+import EletronicPoint from './eletronic-point.png';
+import BatteriePoint from './batterie-point.png';
+import TissuePoint from './tissue-point.png';
+import LightPoint from './light-point.png';
 
 import RecyclingCollectorPoint from './recycling-collector-point.png';
 import CompanyPoint from './company-point.png';
@@ -14,8 +17,8 @@ export const MarkerPointCollect = {
   glass: GlassPoint,
   plastic: PlasticPoint,
   organic: OrganicPoint,
-  light: EletronicPoint,
-  tissue: EletronicPoint,
-  batterie: EletronicPoint,
+  light: LightPoint,
+  tissue: TissuePoint,
+  batterie: BatteriePoint,
   eletronic: EletronicPoint,
 };
