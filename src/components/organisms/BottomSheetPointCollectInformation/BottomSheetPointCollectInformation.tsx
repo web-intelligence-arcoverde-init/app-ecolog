72d953cb783b18d9dc1b4daa3a77344093abfe43
Lucas Henrique <lukas.paes18@gmail.com>
2022-11-10 00:20:38 -0300

    feat: scroll bottomsheet

--- src/components/organisms/BottomSheetPointCollectInformation/BottomSheetPointCollectInformation.tsx
+++ src/components/organisms/BottomSheetPointCollectInformation/BottomSheetPointCollectInformation.tsx
@@ -1,6 +1,9 @@
 import React, {useMemo, useState} from 'react';
 
-import BottomSheet, {useBottomSheetSpringConfigs} from '@gorhom/bottom-sheet';
+import BottomSheet, {
+  useBottomSheetSpringConfigs,
+  BottomSheetScrollView,
+} from '@gorhom/bottom-sheet';
 import {ScrollView, View} from 'react-native';
 
 import {Typography, Separator, Button} from '../../';
@@ -43,77 +46,75 @@ export const BottomSheetPointCollectInformation = () => {
       enableContentPanningGesture={enableContentPanningGesture}
       enableHandlePanningGesture={enableHandlePanningGesture}>
       {!isSelectedPoint && (
-        <View
+        <BottomSheetScrollView
           style={{
             flex: 1,
             paddingHorizontal: scale(24),
-            justifyContent: 'flex-end',
           }}>
-          <ScrollView showsVerticalScrollIndicator={false}>
-            <Separator height={16} />
+          <Separator height={16} />
 
-            <Typography>Ponto de reciclagem</Typography>
+          <Typography>Ponto de reciclagem</Typography>
 
-            <Separator height={8} />
+          <Separator height={8} />
 
-            <ItemTypeTitle point={pointSelected} />
+          <ItemTypeTitle point={pointSelected} />
 
-            <Separator height={16} />
+          <Separator height={16} />
 
+          <View>
+            <Typography variant="body" color="gray-500">
+              Nome:
+            </Typography>
+            <Separator height={4} />
+
+            <Typography variant="body">Lucas Henrique</Typography>
+          </View>
+
+          <Separator height={8} />
+
+          <View
+            style={{
+              display: 'flex',
+              flexDirection: 'row',
+              justifyContent: 'space-between',
+            }}>
             <View>
               <Typography variant="body" color="gray-500">
-                Nome:
+                Data de criação:
               </Typography>
               <Separator height={4} />
-
-              <Typography variant="body">Lucas Henrique</Typography>
+              <Typography variant="body">21/06/2022</Typography>
             </View>
-
-            <Separator height={8} />
-
-            <View
-              style={{
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-between',
-              }}>
-              <View>
-                <Typography variant="body" color="gray-500">
-                  Data de criação:
-                </Typography>
-                <Separator height={4} />
-                <Typography variant="body">21/06/2022</Typography>
-              </View>
-              <View>
-                <Typography variant="body" color="gray-500">
-                  Contato:
-                </Typography>
-                <Separator height={4} />
-                <Typography variant="body">879998093765</Typography>
-              </View>
-            </View>
-            <Separator height={8} />
-
             <View>
               <Typography variant="body" color="gray-500">
-                Descrição:
+                Contato:
               </Typography>
               <Separator height={4} />
-              <View>
-                <Typography variant="legend">
-                  Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Laborum optio natus inventore, dolor et distinctio at?
-                  Consequuntur voluptatem nemo illo quis? Eius nesciunt ea
-                  repudiandae voluptatem quisquam nobis tempora molestias!
-                </Typography>
-              </View>
+              <Typography variant="body">879998093765</Typography>
             </View>
+          </View>
+          <Separator height={8} />
 
-            <Separator height={24} />
-            <Button color="error-100">Remover</Button>
-            <Separator height={40} />
-          </ScrollView>
-        </View>
+          <View>
+            <Typography variant="body" color="gray-500">
+              Descrição:
+            </Typography>
+            <Separator height={4} />
+            <View>
+              <Typography variant="legend">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum
+                optio natus inventore, dolor et distinctio at? Consequuntur
+                voluptatem nemo illo quis? Eius nesciunt ea repudiandae
+                voluptatem quisquam nobis tempora molestias!
+              </Typography>
+            </View>
+          </View>
+
+          <Separator height={24} />
+          <Button color="error-100">Remover</Button>
+
+          <Separator height={32} />
+        </BottomSheetScrollView>
       )}
     </BottomSheet>
   );
